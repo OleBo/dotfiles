5b4beb0c4a89763c72f8db56c7ea466dbc15c774
Olaf Bochmann <Olaf.Bochmann@gmail.com>
2020-11-17 06:18:08 +0100

    add div. packages

--- packages.sh
+++ packages.sh
@@ -5,18 +5,43 @@ setopt sh_word_split
 
 # Packages installed from Brew.
 BREW_PACKAGES="
+ack
+awscli
 colordiff
+colortail
+graphviz
 grc
 htop
+imagemagick
+jq
 neovim
+p7zip
+pandoc
 python
+rclone
+testdisk
+tldr
+tree
+unrar
 wget
 "
 
 # Packages to install by the way of Brew's casks.
 CASK_PACKAGES="
+bisq
+db-browser-for-sqlite
+disk-drill
+dropbox
+dupeguru
+electrum
+fork
+gimp
+google-drive-file-stream
 iina
+java
+kap
 macdown
+netnewswire
 tor-browser
 "
 
@@ -24,4 +49,7 @@ tor-browser
 PYTHON_PACKAGES="
 howdoi
 neovim
+pycodestyle
+pydocstyle
+pygments
 "
